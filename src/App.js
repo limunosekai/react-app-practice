87b964af2ed1db182cf550a80bca905855c30fda
limunosekai <powerlsh0103@gmail.com>
2021-02-02 00:52:40 +0900

    POST orders to firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-// import Layout from './section8/hoc/Layout/Layout';
-// import BurgerBuilder from './section8/containers/BurgerBuilder/BurgerBuilder';
-import Blog from './section9/containers/Blog/Blog';
+import Layout from './section8/hoc/Layout/Layout';
+import BurgerBuilder from './section8/containers/BurgerBuilder/BurgerBuilder';
+// import Blog from './section9/containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      // <div>
-      //   <Layout>
-      //     <BurgerBuilder />
-      //   </Layout>
-      // </div>
-      <div className='App'>
-        <Blog />
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
+      // <div className='App'>
+      //   <Blog />
+      // </div>
     );
   }
 }
--- src/section8/containers/BurgerBuilder/BurgerBuilder.js
+++ src/section8/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICE = {
   salad: 300,
@@ -23,6 +25,7 @@ class BurgerBuilder extends Component {
     totalPrice: 2900,
     purchasable: false,
     purchasing: false,
+    loading: false,
   };
 
   updatePurchaseState = (ingredients) => {
@@ -85,7 +88,29 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('계속합니다');
+    this.setState({ loading: true });
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Lim Seonghyeon',
+        address: {
+          street: 'TestStreet 1',
+          zipCode: '12345',
+          country: 'Korea',
+        },
+        email: 'test@test.com',
+      },
+      deliveryMethod: 'walk',
+    };
+    axios
+      .post('/orders.json', order)
+      .then((response) => {
+        this.setState({ loading: false, purchasing: false });
+      })
+      .catch((error) => {
+        this.setState({ loading: false, purchasing: false });
+      });
   };
 
   render() {
@@ -97,18 +122,24 @@ class BurgerBuilder extends Component {
       // true or false boolean 저장
       disabledInfo[key] = disabledInfo[key] <= 0;
     }
+    let orderSummary = (
+      <OrderSummary
+        ingredients={this.state.ingredients}
+        price={this.state.totalPrice}
+        purchaseCancelled={this.purchaseCancelHandler}
+        purchaseContinued={this.purchaseContinueHandler}
+      />
+    );
+    if (this.state.loading) {
+      orderSummary = <Spinner />;
+    }
     return (
       <Auxiliary>
         <Modal
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}
         >
-          <OrderSummary
-            ingredients={this.state.ingredients}
-            price={this.state.totalPrice}
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler}
-          />
+          {orderSummary}
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
