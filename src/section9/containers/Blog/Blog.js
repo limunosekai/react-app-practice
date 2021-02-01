e2d73cca517ac12ce388e06a897fafa97ace5129
limunosekai <powerlsh0103@gmail.com>
2021-02-01 23:10:56 +0900

    section9 HTTP Request

--- src/section9/containers/Blog/Blog.js
+++ src/section9/containers/Blog/Blog.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-
+// import axios from 'axios';
+import axios from '../../axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false,
+  };
+
+  componentDidMount() {
+    axios
+      .get('/posts')
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatePosts = posts.map((post) => {
+          return {
+            ...post,
+            author: 'Lim',
+          };
+        });
+        this.setState({
+          posts: updatePosts,
+        });
+      })
+      .catch((error) => {
+        this.setState({ error: true });
+      });
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    let posts = <p styles={{ textAlign: 'center' }}>ERROR</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
         return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
         );
+      });
     }
+
+    return (
+      <div>
+        <section className='Posts'>{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
