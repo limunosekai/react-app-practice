95884a026d1f5ee176dc567198b934df2e405e14
limunosekai <powerlsh0103@gmail.com>
2021-02-01 17:57:51 +0900

    section9 initialize

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import Layout from './section8/hoc/Layout/Layout';
-import BurgerBuilder from './section8/containers/BurgerBuilder/BurgerBuilder';
+// import Layout from './section8/hoc/Layout/Layout';
+// import BurgerBuilder from './section8/containers/BurgerBuilder/BurgerBuilder';
+import Blog from './section9/containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
+      // <div>
+      //   <Layout>
+      //     <BurgerBuilder />
+      //   </Layout>
+      // </div>
+      <div className='App'>
+        <Blog />
       </div>
     );
   }
