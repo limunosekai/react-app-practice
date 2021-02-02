3ba70b7f2ad05c481b82eb122802f41bed80940e
limunosekai <powerlsh0103@gmail.com>
2021-02-02 10:28:40 +0900

    Init for section11 Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import Layout from './section8/hoc/Layout/Layout';
-import BurgerBuilder from './section8/containers/BurgerBuilder/BurgerBuilder';
-// import Blog from './section9/containers/Blog/Blog';
+// import Layout from './section8/hoc/Layout/Layout';
+// import BurgerBuilder from './section8/containers/BurgerBuilder/BurgerBuilder';
+import Blog from './section9/containers/Blog/Blog';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </div>
-      // <div className='App'>
-      //   <Blog />
+      // <div>
+      //   <Layout>
+      //     <BurgerBuilder />
+      //   </Layout>
       // </div>
+      <BrowserRouter>
+        <div className='App'>
+          <Blog />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,33 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import axios from 'axios';
+import axios from 'axios';
 
-// axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
-// axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
-// axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-// axios.interceptors.request.use(
-//   (request) => {
-//     console.log(request);
-//     return request;
-//   },
-//   (error) => {
-//     console.log(error);
-//     return Promise.reject(error);
-//   }
-// );
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
-// axios.interceptors.response.use(
-//   (request) => {
-//     console.log(request);
-//     return request;
-//   },
-//   (error) => {
-//     console.log(error);
-//     return Promise.reject(error);
-//   }
-// );
+axios.interceptors.response.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
 ReactDOM.render(
   <React.StrictMode>
