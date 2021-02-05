bb50feb643cfde5b429390dffff8813b574acb13
limunosekai <powerlsh0103@gmail.com>
2021-02-05 21:51:36 +0900

    init section 14

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux;';
+import reducer from './section14/store/reducer';
 // import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
@@ -32,11 +35,16 @@ import { BrowserRouter } from 'react-router-dom';
 //   }
 // );
 
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
