8cf59f1c3d6332fe614817f3568f3a797a228c39
limunosekai <powerlsh0103@gmail.com>
2021-02-21 15:09:11 +0900

    로그인 유지, 보안 강화

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Layout from './section8/hoc/Layout/Layout';
 import BurgerBuilder from './section8/containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './section8/containers/Checkout/Checkout';
 import Orders from './section8/containers/Orders/Orders';
-import { Route, Switch } from 'react-router-dom';
 import Auth from './section8/containers/Auth/Auth';
 import logout from './section8/containers/Auth/Logout/Logout';
+import * as actions from './section8/store/actions/index';
 
 // import Blog from './section9/containers/Blog/Blog';
 // import Control from './section11/Control';
 // import Counter from './section14/containers/Counter/Counter';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
   render() {
+    // 인증 안된 사용자 접속시
+    let routes = (
+      <Switch>
+        <Route path='/auth' component={Auth} />
+        <Route path='/' exact component={BurgerBuilder} />
+        <Redirect to='/' />
+      </Switch>
+    );
+
+    // 인증된 사용자 접속시
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
+          <Route path='/logout' component={logout} />
+          <Route path='/' exact component={BurgerBuilder} />
+          <Redirect to='/' />
+        </Switch>
+      );
+    }
+
     return (
       <div>
-        <Layout>
-          <Switch>
-            <Route path='/checkout' component={Checkout} />
-            <Route path='/orders' component={Orders} />
-            <Route path='/auth' component={Auth} />
-            <Route path='/logout' component={logout} />
-            <Route path='/' exact component={BurgerBuilder} />
-          </Switch>
-        </Layout>
+        <Layout>{routes}</Layout>
       </div>
       // <BrowserRouter>
       //   <div className='App'>
@@ -37,4 +56,16 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
